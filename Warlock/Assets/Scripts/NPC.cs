a5cb74daa6e7158f6e39344da77334681b891bad
Herctic <lin.alex@utexas.edu>
2018-11-12 15:33:40 -0600

    Removed old art assets

---
+++ Warlock/Assets/Scripts/NPC.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPC : MonoBehaviour {
+
+	// Use this for initialization
+
+	public GameObject[] targets;
+	private int currentTarget;
+	private int nextTarget;
+
+	private NavMeshAgent me;
+	public float speed;
+
+	void Start () {
+		
+		me = GetComponent<NavMeshAgent>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		speed = me.velocity.magnitude;
+		me.destination = targets[currentTarget].transform.position;
+
+		if (Vector3.Distance(transform.position, targets[currentTarget].transform.position) == 0)
+		{
+			GetComponent<Animator>().SetBool("Walking", false);
+
+		}
+
+
+	}
+	public void ProgressToNext()
+	{
+
+		GetComponent<Animator>().SetBool("Walking", true);
+		nextTarget = Random.Range(0, targets.Length);
+		while(nextTarget == currentTarget)
+		{
+			nextTarget = Random.Range(0, targets.Length);
+		}
+
+		// if(currentTarget >= targets.Length)
+		// {
+		// 	currentTarget = 0;
+		// }
+
+	}
+}
