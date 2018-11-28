3eea04f9dc03585f15f9629e6f3da908e78c4761
Darwin Hnederson <Darwinphenderson@gmail.com>
2018-11-28 10:48:56 -0600

    SPIDER ATTACK

---
+++ Warlock/Assets/Scripts/attack_collider.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attack_collider : MonoBehaviour {
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+            
+        {
+            Animator ani = gameObject.GetComponentInParent(typeof(Animator)) as Animator;
+            ani.SetBool("playerClose",true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+
+    {
+        if (other.tag == "Player")
+
+        {
+            Animator ani = gameObject.GetComponentInParent(typeof(Animator)) as Animator;
+            ani.SetBool("playerClose", false);
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
