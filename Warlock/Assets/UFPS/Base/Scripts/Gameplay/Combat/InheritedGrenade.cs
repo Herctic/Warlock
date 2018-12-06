53493e8bd8cc87bd3713bb10219c0cb78357ae66
Herctic <lin.alex@utexas.edu>
2018-12-05 19:07:51 -0600

    fml

---
+++ Warlock/Assets/UFPS/Base/Scripts/Gameplay/Combat/InheritedGrenade.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InheritedGrenade : vp_Grenade {
+	private vp_Timer.Handle Timer = new vp_Timer.Handle();
+	void OnDestroy(){
+		Timer.Cancel();
+	}
+	void Start () {
+		vp_Timer.In(LifeTime, delegate()
+			{
+				transform.SendMessage("DieBySources", new Transform[]{m_Source, m_OriginalSource}, SendMessageOptions.DontRequireReceiver);
+			},Timer);
+	}
+}
