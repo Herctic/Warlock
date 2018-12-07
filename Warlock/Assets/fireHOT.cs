218c3ebf1959f207e7eb7f60615cc46997e0a40c
Darwin Hnederson <Darwinphenderson@gmail.com>
2018-12-07 16:27:13 -0600

    fireHOT

---
+++ Warlock/Assets/fireHOT.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireHOT : MonoBehaviour {
+    
+    float timer = 0;
+    float damageTime = 2;
+
+
+    void update(){
+       
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if (timer >= damageTime)
+            {
+                
+                vp_LocalPlayer.Damage(1);
+                timer = 0;
+            }
+        }
+
+        timer += Time.deltaTime;
+    }
+}
