3f52f662d6557b0109b3dc00c77af00a364b6b45
Darwin Hnederson <Darwinphenderson@gmail.com>
2018-12-10 00:37:04 -0600

    Monster health adn win screen baby

---
+++ Warlock/Assets/Scripts/WinCondition.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WinCondition : MonoBehaviour {
+
+
+    private GameObject monsters;
+    public Text winText;
+    public Text playAgain;
+
+	// Use this for initialization
+	void Start () {
+        winText.text = "";
+        playAgain.text = "";
+        monsters = this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(monsters.transform.childCount == 0){
+            winText.text = "You Win!";
+            playAgain.text = "Press space to play again!";
+            if (Input.GetKeyDown("space")){
+                SceneManager.LoadScene(0);
+            }
+               
+        }
+	}
+}
