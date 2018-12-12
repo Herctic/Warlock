b3f3e15c776a8014bc0eb99c2d9a263f9ae0c6b0
Herctic <lin.alex@utexas.edu>
2018-12-11 18:53:11 -0600

    brutalist code

---
+++ Warlock/Assets/Tracker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tracker : MonoBehaviour {
+
+	public GameObject spider;
+	public GameObject spider1;
+	public GameObject spider2;
+	public GameObject bones;
+	public GameObject bones1;
+	public GameObject dragon;
+
+	public GameObject ispider;
+	public GameObject ispider1;
+	public GameObject ispider2;
+	public GameObject ibones;
+	public GameObject ibones1;
+	public GameObject idragon;
+
+	void Update () {
+		if (spider == null) {
+			ispider.SetActive(false);
+		}
+		if (spider1 == null) {
+			ispider1.SetActive(false);
+		}
+		if (spider2 == null) {
+			ispider2.SetActive(false);
+		}
+		if (bones == null) {
+			ibones.SetActive(false);
+		}
+		if (bones == null) {
+			ibones1.SetActive(false);
+		}
+		if (dragon == null) {
+			idragon.SetActive(false);
+		}
+	}
+
+}
